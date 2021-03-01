e1f93ee5e8364da888c56f17db69347a9dde0d2b
FloreauLuca <lucafloreau@akeonet.com>
2021-03-01 15:31:02 +0100

    Auto-close test

--- common/aer_lib/test/test_scene.cpp
+++ common/aer_lib/test/test_scene.cpp
@@ -111,7 +111,7 @@ public:
     #ifdef EASY_PROFILE_USE
         EASY_BLOCK("Test Update", profiler::colors::Green);
     #endif
-		//updateCount_++;
+		updateCount_++;
 		if (updateCount_ == kEngineDuration_) { engine_.Stop(); }
 	}
 
